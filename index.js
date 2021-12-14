8b89e2374d609ad9bae55fe84e1a91d1421c6242
Omkar Desai <omkar.desai916@gmail.com>
2021-12-14 20:41:32 +0530

    init

--- index.js
+++ index.js
@@ -2,11 +2,40 @@ const btn = document.querySelector("#addBtn");
 const title = document.querySelector("#noteTitle");
 const dis = document.querySelector("#noteDiscription");
 const form = document.querySelector("#form");
+let cards = [];
 
 form.addEventListener("submit", submitHandler);
 
 function submitHandler(e) {
   e.preventDefault();
-  console.log(e.target[0].value);
-  console.log(e.target[1].value);
+
+  const card = `
+    <div class="card" >
+    <h2>${e.target[0].value}</h2>
+    <p>${e.target[1].value}</p>
+    <button class='delead'id=${cards.length}>DEL</button>
+    </div>
+`;
+
+  cards.unshift(card);
+  rerender();
+  document.querySelector("#mainCard").innerHTML = cards;
+  document.querySelectorAll(".delead").forEach((a) => {
+    a.addEventListener("click", delHandler);
+  });
+}
+function delHandler(e) {
+  console.log(e.target.id);
+  cards = cards.filter(function (item, i) {
+    return i != e.target.id;
+  });
+  rerender();
+  document.querySelectorAll(".delead").forEach((a) => {
+    a.addEventListener("click", delHandler);
+  });
+}
+function rerender() {
+  document.querySelector("#mainCard").innerHTML = cards;
+  title.value = "";
+  dis.value = "";
 }
